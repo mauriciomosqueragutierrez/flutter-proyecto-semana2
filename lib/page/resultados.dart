cebe14c5be881d98857430c3d5e5985751ee14ee
mauriciomosqueragutierrez <mauricio.mosquera.gutierrez@gmail.com>
2021-05-01 17:49:53 -0500

    Front calculadora y resultados maquetación

---
+++ lib/page/resultados.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ResultadosPage extends StatelessWidget {
+
+// Página principal para resultados
+
+  //final List<Text> listadoResultados;
+  //ResultadosPage({{@required this.listadoResultados}});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Resultado de cálculos"),
+      ),
+      body: Column(
+          //children: listadoResultados,
+          ),
+    );
+  }
+}
