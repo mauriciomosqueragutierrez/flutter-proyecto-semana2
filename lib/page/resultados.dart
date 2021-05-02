ed2fb2b9cdce162d52c15c1920765d8e6c9568bd
mauriciomosqueragutierrez <mauricio.mosquera.gutierrez@gmail.com>
2021-05-02 01:00:05 -0500

    Resultados validaciones  cálculos

--- lib/page/resultados.dart
+++ lib/page/resultados.dart
@@ -1,21 +1,60 @@
 import 'package:flutter/material.dart';
+import 'home-page.dart';
 
 class ResultadosPage extends StatelessWidget {
+  var resultado;
 
-// Página principal para resultados
-
-  //final List<Text> listadoResultados;
-  //ResultadosPage({{@required this.listadoResultados}});
+  ResultadosPage({required this.resultado});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Resultado de cálculos"),
+        title: Text("Resultado De Operaciones"),
       ),
-      body: Column(
-          //children: listadoResultados,
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Container(
+          margin: EdgeInsets.all(15),
+          child: Row(
+            children: [
+              Text("Resultado",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold)),
+            ],
           ),
+        ),
+
+
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MyHomePage(
+                          title: 'Calculadora IMC',
+                        )));
+          },
+          child: Container(
+              height: 80,
+              color: Colors.pink,
+              child: Column(children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 22),
+                        ),
+                      ),
+                    ])
+              ])),
+        ),
+      ]),
     );
   }
 }
