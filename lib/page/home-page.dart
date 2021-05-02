7ec2d2c11c24f9b101f7a3daab5f1927888e797d
mauriciomosqueragutierrez <mauricio.mosquera.gutierrez@gmail.com>
2021-05-01 22:25:04 -0500

    Ajuste front dimensiones

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -106,14 +106,11 @@ class _MyHomePageState extends State<MyHomePage> {
                                 style: TextStyle(
                                     fontWeight: FontWeight.bold, fontSize: 50),
                               ),
-                              Text(" centímetros",
+                              Text("   centímetros",
                                   style: TextStyle(
                                       color: Colors.white30, fontSize: 20))
                             ]),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              SliderTheme(
+                               SliderTheme(
                                 data: SliderThemeData(
                                     activeTrackColor: Colors.white,
                                     thumbColor: Colors.purpleAccent,
@@ -122,23 +119,136 @@ class _MyHomePageState extends State<MyHomePage> {
                                     valueIndicatorColor: Colors.purpleAccent),
                                 child: Slider(
                                   value: _currentSliderValue,
-                                  min: 100,
+                                  min: 50,
                                   max: 200,
-                                  divisions: 100,
+                                  divisions: 150,
                                   label:
                                       _currentSliderValue.round().toString() +
-                                          " centímetros",
+                                          "   centímetros",
                                   onChanged: (double value) {
                                     setState(() {
                                       _currentSliderValue = value;
                                     });
                                   },
                                 ),
-                              )
-                            ])
-                      ]))),
+                              ),
+                      
+                       ]))),
         ),
         
+
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF212122),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "Peso",
+                              style: TextStyle(
+                                  color: Colors.white30, fontSize: 20),
+                            )
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "90",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 50),
+                            )
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            ),
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            )
+                          ])
+                    ])),
+            Container(
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF212122),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "Edad",
+                              style: TextStyle(
+                                  color: Colors.white30, fontSize: 20),
+                            )
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "34",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 50),
+                            )
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            ),
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            )
+                          ])
+                    ])),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+              height: 80,
+              color: Colors.purple,
+              child: Column(children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          "Calcular IMC",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30),
+                        ),
+                      ),
+                    ])
+              ])),
+        ),
       ],
     );
   }
